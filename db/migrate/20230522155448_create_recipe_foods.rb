32f3d72aca4ea0303b4e67d2d690dcfb49ca1153
Uthmanbello <uthmanbello88@gmail.com>
2023-05-23 15:40:44 +0100

    Add recipe_foods reference to recipes

--- db/migrate/20230522155448_create_recipe_foods.rb
+++ db/migrate/20230522155448_create_recipe_foods.rb
@@ -2,7 +2,6 @@ class CreateRecipeFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :recipe_foods do |t|
       t.integer :quantity
-      t.integer :recipe_id
       t.references :food, null: false, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_22_162947) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_22_235835) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,11 +27,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_22_162947) do
 
   create_table "recipe_foods", force: :cascade do |t|
     t.integer "quantity"
-    t.integer "recipe_id"
     t.bigint "food_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "recipe_id", null: false
     t.index ["food_id"], name: "index_recipe_foods_on_food_id"
+    t.index ["recipe_id"], name: "index_recipe_foods_on_recipe_id"
   end
 
   create_table "recipes", force: :cascade do |t|
@@ -43,8 +44,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_22_162947) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "recipe_foods_id", null: false
-    t.index ["recipe_foods_id"], name: "index_recipes_on_recipe_foods_id"
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
@@ -57,12 +56,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_22_162947) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "foods", "users"
   add_foreign_key "recipe_foods", "foods"
-  add_foreign_key "recipes", "recipe_foods", column: "recipe_foods_id"
+  add_foreign_key "recipe_foods", "recipes"
   add_foreign_key "recipes", "users"
 end
