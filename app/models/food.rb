ad94ae8fa0a19db6790a1ab083baecff76e276d2
anas599 <anas599@yahoo.com>
2023-05-25 00:21:27 +0300

    add validations to models

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,7 @@
 class Food < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true
+  validates :measure_unit, presence: true, inclusion: { in: %w[Gram Pound Unit] }
+  validates :price, presence: true, numericality: { only_integer: true }
+  validates :quantity, presence: true, numericality: { only_integer: true }
 end
